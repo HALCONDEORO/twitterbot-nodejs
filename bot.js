4452201070bfbabdba1805b86fa84e808a492dc7
HALCONDEORO <marco.antonio.tevar.asensio@gmail.com>
2021-01-04 13:59:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,110 +1,59 @@
-// Debug flag
-
-var debug = false
-
-// Twitter library
 var Twit = require('twit')
 
-//Check config file is filled.
-var config = require('./config.js')
-if(config.consumer_key == 'blah' ||
-   config.consumer_secret == 'blah' ||
-   config.access_token == 'blah' ||
-   config.access_token_secret == 'blah'){
+var arrayWords = [
+    "Teniendo en cuenta que ya hay una película (Jazz y chistes, la película, el podcast), el Kikeverso, es ya oficialmente un Universo Cinematográfico Kike.","No descarto que en el Kikeverso haya un Kike relleno de nata y también sería un Kike, evidentemente.","Cuándo se cambia la hora, esto es inaguantable","Joder qué asco. Ya no digo nada más de esto hasta diciembre, perdonadme"]
 
-	console.log("You must fill your configuration file which is located /config.js");
-	return;
+var T = new Twit({
+    consumer_key: 'd77X8QzbMz6y8qbgmdGGEczxf',
+    consumer_secret: 'GmipG5l6PSsNRrU5I1D62kI4qzCMdNv1Mx36qPt3Obmg4DhFu7',
+    access_token: '1345829866076327938-OZ7rU6jhp1IevsRrnJuFyLKWxpQzWs',
+    access_token_secret: 'VoRQhBeQTo3HpomWi7t6NeHdN81A5X3Mvrw1YRbknlJtr'
+})
+
+//Verificamos las credenciales
+T.get('account/verify_credentials', {
+    include_entities: false,
+    skip_status: true,
+    include_email: false
+})
+
+// Seteamos el stream y usamos la api statues/filter para estar escuchando las menciones a nuestro Bot (en mi caso BotBro)
+var stream = T.stream('statuses/filter', { track: '@kikegdelarivaB1' });
+// Ahora estamos observando todos los eventos relacionados al stream y en caso que pase algo ejecutamos la función tweetEvent que creamos nosotros
+stream.on('tweet', tweetEvent);
+
+//Función tweetEvent
+function tweetEvent(tweet) {
+
+    // Quién envió el Tweet?
+    var name = tweet.user.screen_name;
+    // Si queremos ver el texto del tweet
+    // var txt = tweet.text;
+    // Obtenemos el tweet id
+    var nameID = tweet.id_str;
+
+    // Si queremos eliminar la mención
+    // var txt = txt.replace(/@myTwitterHandle/g, "");
+
+    //Obtenemos un valor Random para buscar en el array
+    var random = getRandomArbitrary(0, arrayWords.length);
+    // Lo que hacemos es responder a ese tweet
+    var reply = "@" + name + " " + arrayWords[random];
+    var params = {
+        status: reply,
+        in_reply_to_status_id: nameID
+    };
+
+    T.post('statuses/update', params, function (err, data, response) {
+        if (err !== undefined) {
+            console.log(err);
+        } else {
+            console.log('Tweeted: ' + params.status);
+        }
+    })
+};
+
+//Funcion para obtener valor Random
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-// We need to include our configuration file
-var T = new Twit(require('./config.js'))
-
-// This is the URL of a search for the latest tweets on the #hashtag.
-var hastagSearch = { q: '#hashtag', count: 10, result_type: 'recent' }
-
-// A user stream
-var stream = T.stream('user')
-// When someone follows the user
-stream.on('follow', followed)
-stream.on('tweet', tweetEvent)
-
-// In this callback we can see the name and screen name
-function followed (event) {
-  var name = event.source.name
-  var screenName = event.source.screen_name
-  var response = 'Thanks for following me, ' + name + ' @' + screenName
-  // Post that tweet!
-  T.post('statuses/update', { status: response }, tweeted)
-
-  console.log('I was followed by: ' + name + ' @' + screenName)
-}
-
-// Here a tweet event is triggered!
-function tweetEvent (tweet) {
-  // If we wanted to write a file out
-  // to look more closely at the data
-  // var fs = require('fs')
-  // var json = JSON.stringify(tweet,null,2)
-  // fs.writeFile("tweet.json", json, output)
-
-  // Who is this in reply to?
-  var reply_to = tweet.in_reply_to_screen_name
-  // Who sent the tweet?
-  var name = tweet.user.screen_name
-  // What is the text?
-  var txt = tweet.text
-
-  // Ok, if this was in reply to me
-  // Replace selftwitterhandle with your own twitter handle
-  console.log(reply_to, name, txt)
-  if (reply_to === 'selftwitterhandle') {
-
-    // Get rid of the @ mention
-    txt = txt.replace(/@selftwitterhandle/g, '')
-
-    // Start a reply back to the sender
-    var reply = 'Hi @' + name + ' ' + ', Thanks for the mention :)'
-
-    console.log(reply)
-    // Post that tweet!
-    T.post('statuses/update', { status: reply }, tweeted)
-  }
-}
-
-// This function finds the latest tweet with the #hashtag, and retweets it.
-function retweetLatest () {
-  T.get('search/tweets', hastagSearch, function (error, data) {
-    var tweets = data.statuses
-    for (var i = 0; i < tweets.length; i++) {
-      console.log(tweets[i].text)
-    }
-    // If our search request to the server had no errors...
-    if (!error) {
-      // ...then we grab the ID of the tweet we want to retweet...
-      var retweetId = data.statuses[0].id_str
-      // ...and then we tell Twitter we want to retweet it!
-      T.post('statuses/retweet/' + retweetId, {}, tweeted)
-    }
-    // However, if our original search request had an error, we want to print it out here.
-    else {
-      if (debug) {
-        console.log('There was an error with your hashtag search:', error)
-      }
-    }
-  })
-}
-
-// Make sure it worked!
-function tweeted (err, reply) {
-  if (err !== undefined) {
-    console.log(err)
-  } else {
-    console.log('Tweeted: ' + reply)
-  }
-}
-
-// Try to retweet something as soon as we run the program...
-retweetLatest()
-// ...and then every hour after that. Time here is in milliseconds, so
-// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 12)
